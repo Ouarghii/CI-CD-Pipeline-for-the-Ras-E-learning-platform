ab92c904cc98235cd073c4368b63520e05a4c3cb
Raslen Warghui <Raslen.ouarghi2018@gmail.com>
2023-08-25 16:48:39 +0100

    update pipeline

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    testEnvironment: 'jsdom', // Use jsdom as the testing environment
-    roots: ['<rootDir>/src'], // Specify the directory containing your test files
-    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'], // Specify the test file patterns
+    testEnvironment: 'jsdom',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
     transform: {
-      '^.+\\.jsx?$': 'babel-jest', // Transform JavaScript files using Babel
+      '^.+\\.jsx?$': 'babel-jest',
     },
   };
   
