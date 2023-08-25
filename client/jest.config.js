53d4fe2a2239197245bf13c240d692dc9f12552b
Raslen Warghui <Raslen.ouarghi2018@gmail.com>
2023-08-25 16:51:10 +0100

    update

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,9 +1,13 @@
-module.exports = {
-    testEnvironment: 'jsdom',
-    roots: ['<rootDir>/src'],
-    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+export default {
     transform: {
       '^.+\\.jsx?$': 'babel-jest',
+      '^.+\\.m?js$': 'babel-jest',
     },
+    moduleNameMapper: {
+      '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    },
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['./src/setupTests.js'], // Replace with the path to your setupTests file
+    moduleDirectories: ['node_modules', 'src'],
   };
   
